7d0796945706b49dba5230b388008d72e56ffe75
Mohammed gaffer <the-100joker@live.com>
2023-11-13 01:12:51 +0300

    Initial commit

---
+++ ServerSidePaginationUsingSP/Scripts/ServerSide.js
@@ -0,0 +1,24 @@
+﻿GetDataTableData();
+function GetDataTableData() {
+    $("#data").DataTable({
+        "processing": true,
+        "serverSide": true,
+        "filter": true,
+        "orderMulti": false,
+        "destroy": true,
+        "ordering": true,
+        "ajax": {
+            "url": '/Home/GetDetails',
+            "type": "POST",
+            "datatype": "json"
+        },
+
+        "columns": [
+            { "data": "First_Name", "name": "First_Name", "autoWidth": true }
+            , { "data": "Last_Name", "name": "Last_Name", "autoWidth": true }
+            , { "data": "Email_Address", "name": "Email_Address", "autoWidth": true }
+            , { "data": "Created_Date", "name": "Created_Date", "autoWidth": true }
+            , { "data": "TotalRecords", "name": "TotalRecords", "autoWidth": true }
+        ]
+    });
+}
